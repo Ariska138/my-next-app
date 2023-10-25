ad3742f7cc792e038a858e5c61e073754b7f8557
Ariska Hidayat <ariska@qolaq.org>
2023-10-25 11:14:53 +0700

    first commit

---
+++ src/pages/api/hello.js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  console.log(req.method); //POST
+  console.log(req.body); //{ NIS: '11223', displayName: 'admin', password: '123456' }
+  //App.post
+  if (req.method === 'POST') {
+    // jalankan
+    res.status(200).json({ hello: 'Hello ' + req.body.displayName });
+  } else {
+    res.status(404).json({ error: true, message: 'mehtod tidak diijinkan' });
+  }
+}
+
